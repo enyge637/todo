49f2e31b8475450ec0f7b4d348e9c40344a6fbe6
enyge637 <enyge637@gmail.com>
2021-06-08 15:46:41 +0900

    upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+import "./App.css";
+import TodoCreate from "./components/TodoCreate";
+import TodoHead from "./components/TodoHead";
+import TodoList from "./components/TodoList";
+import TodoTemplate from "./components/TodoTemplate";
+import { TodoProvider } from "./TodoContext";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #e0ecef;
+  }
+`;
+
+function App() {
+  return (
+    <>
+      <TodoProvider>
+        <GlobalStyle />
+        <TodoTemplate>
+          <TodoHead />
+          <TodoList />
+          <TodoCreate />
+        </TodoTemplate>
+      </TodoProvider>
+    </>
+  );
+}
+
+export default App;
